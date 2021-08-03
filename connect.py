feb21f055209d6eafc54b41ccf0cd1930d29be29
gexpose <gammaexposure@protonmail.com>
2021-08-02 23:54:01 -0400

    .

--- connect.py
+++ connect.py
@@ -3,9 +3,7 @@ from synack import synack
 import sys
 
 s1 = synack()
-s1.headless = True
+s1.headless = False
 s1.configFile = "~/.synack/synack.conf"
 s1.connectToPlatform()
 s1.getSessionToken()
-s1.getAllTargets()
-#s1.getProfile()
