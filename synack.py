45d8eddfd2ee093685703d43009524c34becefc9
gexpose <gammaexposure@protonmail.com>
2021-10-28 20:55:38 +0000

    fixed case sensitivity

--- synack.py
+++ synack.py
@@ -234,11 +234,12 @@ class synack:
 ########################################
     def getCodenames(self, category, mission_only=False):
         categories = ("web application", "re", "mobile", "host", "source code","hardware")
-        if category.lower() == "web":
+        category = category.lower()
+        if category == "web":
             category = "web application"
-        if category.lower() == "re":
+        if category == "re":
             category = "reverse engineering"
-        if category.lower() == "sourcecode":
+        if category == "sourcecode":
             category = "source code"
         if category not in categories:
             raise Exception("Invalid category.")
@@ -631,21 +632,11 @@ class synack:
         csrf_token = m.group(1)
         self.webheaders['X-CSRF-Token'] = csrf_token
 
-        # fix broken Incapsula cookies
-        for cookie_name in self.session.cookies.iterkeys():
-            cookie_value = self.session.cookies.get(cookie_name)
-            if cookie_value.find("\r") > -1 or cookie_value.find("\n") > -1:
-                print("Fixing cookie %s" % cookie_name)
-                cookie_value = re.sub("\r\n *","", cookie_value)
-                cookie_obj = requests.cookies.create_cookie(name=cookie_name,value=cookie_value,path="/")
-                self.session.cookies.clear(domain="login.synack.com",path="/",name=cookie_name)
-                self.session.cookies.set_cookie(cookie_obj)
-
         data={"email":self.email,"password":self.password}
         response = self.try_requests("POST", "https://login.synack.com/api/authenticate", 1, data)
         jsonResponse = response.json()
         if not jsonResponse['success']:
-            print("Error logging in: "+jsonResponse["error"])
+            print("Error logging in: "+jsonResponse)
             return False
         
         progress_token = jsonResponse['progress_token']
