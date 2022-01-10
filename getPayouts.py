5b92bc65434fe530f59b940442d43004eaaacb54
gexpose <gammaexposure@protonmail.com>
2022-01-10 04:32:57 +0000

    removed imports

--- getPayouts.py
+++ getPayouts.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
 from synack import synack
-import psycopg2
-import subprocess
-import os
-import sys
 
 s1 = synack()
+s1.gecko = False
 s1.getSessionToken()
 
 payouts = s1.getTransactions()
